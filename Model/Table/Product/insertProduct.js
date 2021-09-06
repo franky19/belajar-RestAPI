b88c4753832b1228dfe064311a59bd92b2bc4bd1
franky19 <frankimarbun@gmail.com>
2021-09-06 16:36:26 +0700

    insert five data product to database

---
+++ Model/Table/Product/insertProduct.js
@@ -0,0 +1,18 @@
+var db = require("../../../index");
+
+db.connect(function(err) {
+    if (err) throw err;
+    
+    let sql = "INSERT INTO product (product_name,product_price) VALUES ?";
+    var values = [
+        ['Product 1','2000'],
+        ['Product 2','5000'],
+        ['Product 3','4000'],
+        ['Product 4','6000'],
+        ['Product 5','7000']
+    ];
+    db.query(sql, [values], function (err, result) {
+        if (err) throw err;
+        console.log("Number of records inserted: " + result.affectedRows);
+    });
+});
